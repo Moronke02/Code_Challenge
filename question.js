dcaa8e1910182ff8c4ff863b359b1a2e66575324
Moronke <moronkealiu02@gmail.com>
2019-03-04 20:49:10 +0100

    question.js

--- question.js
+++ question.js
@@ -1,8 +1,8 @@
 function QuestionMarks(str){
     var ans=false;
     for(var i=0; i<str.length;i++){
-        for(var j=1+1; j<str.length;j++){
-            if(Number(str[i]) + Number(str[j]===10)){
+        for(var j=i+1; j<str.length;j++){
+            if(Number(str[i]) + Number(str[j])===10){
                 ans=true;
                 if(str.slice(i,j).split('?').length-1<3)
                 {
@@ -14,6 +14,4 @@ function QuestionMarks(str){
     return ans;
 }
 console.log(QuestionMarks("aa6?9"))
-console.log(QuestionMarks("acc?7??sss?3rr1??????5"
-
-))
+console.log(QuestionMarks("acc?7??sss?3rr1??????5"))
